82b4269396fb77070c5b4143af01e233dbf89a4e
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-08-14 15:42:19 +0200

    init

--- server/index.js
+++ server/index.js
@@ -1,20 +1,58 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+var fetch = require('node-fetch')
+var axios = require('axios')
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
+    getCompany(name: String!): Company
+  }
+
+  type Company {
+    name: String
   }
 `);
+
+// const companyGetter = async (name) => {
+//   const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${name}&apikey=42342fds234243`
+
+//   axios.get(url).then(res => {
+//     return res.data.bestMatches[0]["2. name"]
+//   })
+// }
 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
     return 'Hello world!';
   },
+  // getCompany: companyGetter(name)
+  getCompany: async (_, {name}) => {
+
+    const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${name}&apikey=42342fds234243`
+    const res = await axios.get(url)
+      .then(function(response) {
+        return response.data;
+      }
+    );
+    // console.log(res.bestMatches[0]["2. name"])
+    return 'lol'
+    // console.log(res.bestMatches[0]["2. name"], 'lollll' );
+    // return res.bestMatches[0]["2. name"]
+
+  }
 };
+
+// const fetchData = async (name) => {
+//   const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${name}&apikey=42342fds234243`
+//   const response = await fetch(url)
+//   const res = await response.json()
+//   console.log(res.bestMatches[0]["2. name"])
+//   return res.bestMatches[0]["2. name"]
+// }
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -23,4 +61,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
+// console.log(fetchData('FB'))
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+
